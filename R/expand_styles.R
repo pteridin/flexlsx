d082c559b74b6e86c0b210c656bed8b21d67e4f2
Tobias Heidler <tobias.heidler@googlemail.com>
2023-08-21 19:42:01 +0200

    Added namespaces

--- R/expand_styles.R
+++ R/expand_styles.R
@@ -7,6 +7,11 @@
 #'
 #' @param df_x styling information incl. col_id & row_id
 #'
+#' @importFrom dplyr select rowwise do ungroup mutate
+#' @importFrom dplyr arrange bind_cols first
+#' @importFrom rlang hash .data
+#' @importFrom openxlsx2 int2col
+#'
 #' @return merged styles as a [tibble][tibble::tibble-package]
 get_dim_ranges <- function(df_x) {
 
@@ -18,24 +23,24 @@ get_dim_ranges <- function(df_x) {
     dplyr::do(hash = rlang::hash(.)) |>
     dplyr::ungroup() |>
     tidyr::unnest_legacy() |>
-    dplyr::mutate(hash = as.integer(factor(hash)))
+    dplyr::mutate(hash = as.integer(factor(.data$hash)))
 
   ## Based on style merge rows -------
   df_aggregated <- df_x |>
     dplyr::bind_cols(df_style_hashed) |>
-    dplyr::arrange(col_id, row_id) |>
-    dplyr::group_by(col_id) |>
-    dplyr::mutate(line = cumsum(dplyr::coalesce(dplyr::lag(hash),0L) - hash != 0)) |>
-    dplyr::group_by(col_id, line) |>
-    dplyr::mutate(row_to = max(row_id)) |>
+    dplyr::arrange(.data$col_id, .data$row_id) |>
+    dplyr::group_by(.data$col_id) |>
+    dplyr::mutate(line = cumsum(dplyr::coalesce(dplyr::lag(.data$hash),0L) - .data$hash != 0)) |>
+    dplyr::group_by(.data$col_id, .data$line) |>
+    dplyr::mutate(row_to = max(.data$row_id)) |>
     dplyr::do(style = dplyr::first(dplyr::select(., -dplyr::all_of(c("col_id", "line"))))) |>
     dplyr::ungroup() |>
     tidyr::unnest_legacy()
 
   ## Prepare ranges --------
   df_aggregated <- df_aggregated |>
-    dplyr::mutate(dims = paste0(openxlsx2::int2col(col_id), row_id, ":",
-                                openxlsx2::int2col(col_id), row_to))
+    dplyr::mutate(dims = paste0(openxlsx2::int2col(.data$col_id), .data$row_id, ":",
+                                openxlsx2::int2col(.data$col_id), .data$row_to))
 
   df_aggregated$multi_lines <- df_aggregated$row_id != df_aggregated$row_to
 
