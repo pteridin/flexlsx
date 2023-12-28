bbfa90c5d44d6ac71a9827ae0f724a84e723294c
Tobias Heidler <tobias.heidler@wig2.de>
2023-12-28 13:56:47 +0100

    Fixed broken check

--- tests/testthat/test-ft_to_style.R
+++ tests/testthat/test-ft_to_style.R
@@ -8,8 +8,9 @@ test_that("ft_to_style_tibble does not break", {
   flexlsx:::ft_to_style_tibble(ft,
                      offset_rows = 0L, offset_cols = 0L, offset_caption_rows = 0L) -> x
 
-  # Fix some platforms that use other default fonts
-  x <- x |> select(-ends_with("family"))
+  # Fix some platforms that use other default fonts & row-heights
+  x <- x |> select(-ends_with("family"),
+                   -all_of("rowheight"))
 
   expect_snapshot_value(as.list(x), style = "json2")
 })
@@ -22,8 +23,9 @@ test_that("ft_to_style_tibble does not break with offsets", {
   flexlsx:::ft_to_style_tibble(ft,
                      offset_rows = 5L, offset_cols = 2L, offset_caption_rows = 8L) -> y
 
-  # Fix some platforms that use other default fonts
-  y <- y |> select(-ends_with("family"))
+  # Fix some platforms that use other default fonts & row-heights
+  y <- y |> select(-ends_with("family"),
+                   -all_of("rowheight"))
 
   expect_snapshot_value(as.list(y), style = "json2")
 })
