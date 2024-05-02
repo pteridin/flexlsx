51f9ac09c5485a8ddfc6a1c388eea7340729c04a
Tobias Heidler <tobias.heidler@wig2.de>
2024-05-02 21:14:43 +0200

    Separated get_dim_ranges into multiple functions

--- R/dim_ranges.R
+++ R/dim_ranges.R
@@ -7,45 +7,104 @@
 #'
 #' @param df_x styling information incl. col_id & row_id
 #'
-#' @importFrom dplyr select do ungroup mutate summarize group_by all_of
+#' @importFrom dplyr select group_by summarize mutate
 #' @importFrom dplyr arrange bind_cols first everything slice_min
 #' @importFrom openxlsx2 int2col
+#' @importFrom rlang .data
 #'
 #' @return merged styles as a [tibble][tibble::tibble-package]
 get_dim_ranges <- function(df_x) {
+  df_hashes <- df |>
+    style_to_hash()
 
-  ## Hash the style information -------
-  df_style_hashed <- df_x |>
-    dplyr::group_by(dplyr::across(-dplyr::all_of(c("col_id",
-                                                   "row_id")))) |>
-    dplyr::summarize(hash = 1L,
-                     .groups = "drop") |>
-    dplyr::mutate(hash = dplyr::row_number())
+  df_rowwise <- df |>
+    get_dim_rowwise(df_hashes)
 
-  cols_to_join <- names(df_style_hashed)[names(df_style_hashed) != "hash"]
+  df_colwise <- df_rowwise |>
+    get_dim_colwise()
 
-
-  ## Aggregate based on style, merge rows -------
-  df_aggregated <- df_x |>
-    dplyr::left_join(df_style_hashed,
-                     by = cols_to_join) |>
-    dplyr::arrange(.data$col_id, .data$row_id) |>
-    dplyr::group_by(.data$col_id) |>
-    dplyr::mutate(line = cumsum(dplyr::coalesce(dplyr::lag(.data$hash),0L) - .data$hash != 0)) |>
-    dplyr::group_by(.data$col_id, .data$line) |>
-    dplyr::mutate(row_to = max(.data$row_id)) |>
-    dplyr::slice_min(.data$row_id) |>
-    dplyr::ungroup()
-
-  ## Prepare ranges --------
-  df_aggregated <- df_aggregated |>
-    dplyr::mutate(dims = paste0(openxlsx2::int2col(.data$col_id), .data$row_id, ":",
-                                openxlsx2::int2col(.data$col_id), .data$row_to)) |>
-    dplyr::select(all_of(c("col_id", "line",   "row_id")),
-                  dplyr::everything())
-
-  df_aggregated$multi_lines <- df_aggregated$row_id != df_aggregated$row_to
-
+  df_aggregated <- df_colwise |>
+    mutate(dims = paste0(openxlsx2::int2col(.data$col_from), .data$row_from,
+                         ":",
+                         openxlsx2::int2col(.data$col_to), .data$row_to),
+           multi_lines = .data$row_to != .data$row_from |
+             .data$col_to != .data$col_from)
 
   return(df_aggregated)
+}
+
+#' Retrieves hashed style information
+#'
+#' Converts each style to an individual integer hash
+#' for easy comparison and aggregation.
+#'
+#' @inheritParams get_dim_ranges
+#'
+#' @return hashed style information as a [tibble][tibble::tibble-package]
+#'
+#' @importFrom dplyr arrange group_by summarize mutate all_of
+#' @importFrom dplyr across row_number
+#'
+style_to_hash <- function(df_x) {
+  df_style_hashed <- df_x |>
+    arrange(across(all_of(c("row_id", "col_id")))) |>
+    group_by(across(-all_of(c("col_id","row_id")))) |>
+    summarize(hash = 1L, .groups = "drop") |>
+    mutate(hash = row_number())
+
+  cols_to_join <- names(df_style_hashed)[names(df_style_hashed) != "hash"]
+  attr(df_style_hashed, "cols_to_join") <- cols_to_join
+  return(df_style_hashed)
+}
+
+
+#' Groups each column with same style each row
+#'
+#' @inheritParams get_dim_ranges
+#' @param df_style_hashed [tibble][tibble::tibble-package] of hashed style
+#' information
+#'
+#' @return [tibble][tibble::tibble-package] of row-wise aggregates style
+#' information
+#'
+#' @importFrom dplyr left_join select arrange group_by
+#' @importFrom dplyr mutate summarize first last
+#' @importFrom dplyr all_of first last across
+#' @importFrom rlang .data
+#'
+get_dim_rowwise <- function(df_x, df_style_hashed) {
+  df_rows <- df_x |>
+    left_join(df_style_hashed,
+              by = attr(df_style_hashed,
+                        "cols_to_join")) |>
+    select(all_of(c("row_id", "col_id", "hash"))) |>
+    arrange(across(all_of(c("row_id", "col_id")))) |>
+    group_by(across(all_of("row_id"))) |>
+    mutate(col_change = cumsum(.data$hash !=
+                                        lag(.data$hash,
+                                            default = first(.data$hash)))) |>
+    group_by(across(all_of(c("row_id", "hash", "col_change")))) |>
+    summarize(col_from = first(col_id),
+                     col_to   = last(col_id),
+                     .groups  = "drop") |>
+    select(-all_of("col_change")) |>
+    arrange(across(all_of(c("row_id", "col_from"))))
+
+  return(df_rows)
+}
+
+
+get_dim_colwise <- function(df_rows) {
+  df_rows |>
+    arrange(across(all_of(c("col_from", "col_to", "row_id")))) |>
+    group_by(across(all_of(c("col_from", "col_to")))) |>
+    mutate(row_change = row_id != lag(row_id, default = first(row_id)) + 1L,
+              style_change = hash != lag(hash, default = first(hash)),
+              change = cumsum(row_change | style_change)) |>
+    group_by(across(all_of(c("hash", "col_from", "col_to", "change")))) |>
+    summarize(row_from = min(row_id),
+               row_to = max(row_id),
+               .groups = "drop") |>
+    select(-all_of("change")) |>
+    arrange(across(all_of(c("row_from", "col_from"))))
 }
--- tests/testthat/test-dim_ranges.R
+++ tests/testthat/test-dim_ranges.R
@@ -1,21 +1,94 @@
 
-test_that("dim_ranges does not break", {
-  tibble::tibble(col_id = c(1, 1, 1, 2, 2, 2),
-                 row_id = c(1, 2, 3, 1, 2, 3),
-                 style = c("a", "a", "b", "a", "b", "b")) -> df
+## Create a test dataset
+matrix(
+  c(".",".",".",".",".",".",
+    "-","-","-","-","-","-",
+    "-",".",".",".",".","-",
+    "-",".",".",".",".","-",
+    "-",".",".",".",".","-",
+    "-","-","-","-","-","-",
+    "-",".","-","-",".","-",
+    "-",".","-","-",".","-",
+    ".",".",".",".",".","."),
+  ncol = 6,
+  byrow = TRUE) -> m
 
-  get_dim_ranges(df) -> df_ranges
-  df_ranges$hash -> hashes
+m |>
+  as.data.frame() |>
+  mutate(row_id = 1:n()) |>
+  tidyr::pivot_longer(cols = -row_id,
+                      names_to = "name",
+                      values_to = "style") |>
+  mutate(col_id = rep(1:6, nrow(m)),
+         style_other = T) |>
+  select(-name) -> df
 
-  expect_equal(hashes[1], hashes[3])
-  expect_equal(hashes[2], hashes[4])
-  expect_equal(df_ranges |>
-                 select(-hash),
+## recreate matrix
+recreate_matrix <- function(df) {
+  if("row_id" %in% names(df)) {
+    df$row_from <- df$row_id
+    df$row_to <- df$row_id
+  }
+
+  df$hash <- factor(df$hash, c(1L,2L), c("-",".")) |>
+    as.character()
 
 
-               tibble::tribble(~col_id,  ~line, ~row_id, ~style, ~row_to, ~dims, ~multi_lines,
-                                 1,      1,     1,  "a",  2,   "A1:A2", TRUE,
-                                 1,      2,     3,  "b",  3,   "A3:A3", FALSE ,
-                                 2,      1,     1,  "a",  1,   "B1:B1", FALSE,
-                                 2,      2,     2,  "b",  3,   "B2:B3", TRUE))
+  m2 <- matrix(NA_character_,
+               nrow = max(df$row_to),
+               ncol = max(df$col_to))
+  for(i in seq_len(nrow(df))) {
+    m2[df$row_from[i]:df$row_to[i],
+       df$col_from[i]:df$col_to[i]] <- df$hash[i]
+  }
+
+  return(m2)
+}
+
+
+test_that("style_to_hash works",
+          {
+
+  df_hashes <- df |>
+    style_to_hash()
+
+  df_reference <- tibble::tribble(
+    ~style, ~style_other, ~hash,
+    "-",    T,            1L,
+    ".",    T,            2L
+  )
+
+  attr(df_reference, "cols_to_join") <- c("style", "style_other")
+
+  testthat::expect_equal(df_hashes,
+                         df_reference)
 })
+
+
+test_that("get_dim_rowwise works", {
+            df_hashes <- df |>
+              style_to_hash()
+
+            df_rowwise <- df |>
+              get_dim_rowwise(df_hashes)
+
+            expect_equal(recreate_matrix(df_rowwise), m)
+})
+
+test_that("get_dim_colwise works",
+          {
+            df_hashes <- df |>
+              style_to_hash()
+
+            df_rowwise <- df |>
+              get_dim_rowwise(df_hashes)
+
+            df_colwise <- df_rowwise |>
+              get_dim_colwise()
+
+
+            expect_equal(recreate_matrix(df_colwise), m)
+
+          })
+
+
