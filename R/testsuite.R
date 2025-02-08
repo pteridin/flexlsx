3bcb98309baa30765e0734aaab7cfb409b465125
Tobias Heidler <tobias.heidler@googlemail.com>
2025-02-08 17:06:26 +0100

    Output based on testsuite5645454652165 variable

--- R/testsuite.R
+++ R/testsuite.R
@@ -1,11 +1,10 @@
 test_wb_ft <- function(wb, ft, filename) {
   # For local development testing only
-  if(exists("testsuite5645454652165") &&
-     testsuite5645454652165 == T) {
-    wb$save(paste0("testsuite/",
+  if(exists("testsuite5645454652165")) {
+    wb$save(paste0(testsuite5645454652165,
                    filename,
                    ".xlsx"))
-    flextable::save_as_html(ft, path = paste0("testsuite/",
+    flextable::save_as_html(ft, path = paste0(testsuite5645454652165,
                                               filename,
                                               ".html"))
   }
